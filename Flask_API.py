230aafd0e56d83a60ef0929113f855a5206f5a7f
ivarvani <i.varvani@gmail.com>
2023-09-20 16:15:18 +1000

    updated databse code for in notebook

--- Flask_API.py
+++ Flask_API.py
@@ -248,17 +248,3 @@ def accidents_by_gender():
 # Start the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
