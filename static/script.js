05a79e928fb40695aa7705ca11c1520f79393104
amcl11 <132857824+amcl11@users.noreply.github.com>
2023-09-20 15:03:59 +1000

    added geojson file to static folder

--- static/script.js
+++ static/script.js
@@ -1,93 +1,81 @@
-// Import lgaColors file
+console.log("Script loaded");
 import { lgaColors } from './lgaColors.js';
 
-// Create map centered at the middle of Victoria, Australia
 let mymap = L.map('mapid').setView([-37.8136, 144.9631], 7);
 
-// Add OpenStreetMap layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-// Link to GeoJSON LGA data
-let link = "https://data.gov.au/geoserver/vic-local-government-areas-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_bdf92691_c6fe_42b9_a0e2_a4cd716fa811&outputFormat=json";
+let link = 'static/vic_lga.geojson';
 
-// Fetch LGA details from Flask API
 let lgaDetails = [];
+
 fetch('/accidents_by_LGA')
   .then(response => response.json())
   .then(data => {
     lgaDetails = data;
+    console.log("Fetched LGA Details:", lgaDetails);
+  })
+  .catch(err => {
+    console.error('Fetch error:', err);
   });
 
-// Function to choose color
-function chooseColor(lgaName) {
-    return lgaColors[lgaName] || '#000000'; // Default to black
+  function chooseColor(lgaName) {
+    const color = lgaColors[lgaName] || '#000000';
+    return color;
 }
 
-// Retrieve GeoJSON data
+
+
 d3.json(link).then(function(data) {
-    L.geoJson(data, {
-        style: function(feature) {
-            return {
-                color: "white",
-                // Call the chooseColor() function to decide color based on LGA Name
-                fillColor: chooseColor(feature.properties.vic_lga__3),
-                fillOpacity: 0.5,
-                weight: 1.5
-            };
+  console.log("Loaded GeoJSON Data:", data);
+  
+  L.geoJson(data, {
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: chooseColor(feature.properties.ABB_NAME),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    onEachFeature: function(feature, layer) {
+      // Bind popup with LGA name for mouseover
+      layer.bindPopup(`<h2 class="popup-title">${feature.properties.ABB_NAME}</h2>`);
+      
+      layer.on({
+        mouseover: function(event) {
+          let hoverLayer = event.target;
+          hoverLayer.setStyle({ fillOpacity: 0.9 });
+          hoverLayer.openPopup();  // Open the popup on mouseover
         },
-        onEachFeature: function(feature, layer) {
-            // Set mouse events to change the map styling
-            layer.on({
-                mouseover: function(event) {
-                    layer = event.target;
-                    layer.setStyle({
-                        fillOpacity: 0.9
-                    });
-                    // Display LGA name on hover
-                    layer.bindPopup('<h2 class="popup-title">' + feature.properties.vic_lga__3 + '</h2>').openPopup();
-                },
-                mouseout: function(event) {
-                    layer = event.target;
-                    layer.setStyle({
-                        fillOpacity: 0.5
-                    });
-                },
-                click: async function(event) {
-                    // Zoom to bounds
-                    mymap.fitBounds(event.target.getBounds());
+        mouseout: function(event) {
+          event.target.setStyle({ fillOpacity: 0.5 });
+          event.target.closePopup();  // Close the popup on mouseout
+        },
+        click: function(event) {
+          let clickLayer = event.target;
+          mymap.fitBounds(clickLayer.getBounds());
 
-                    // Fetch data from the Flask API
-                    let response = await fetch('http://127.0.0.1:5000/accidents_by_LGA');
-                    if (response.status !== 200) {
-                        console.log('Error fetching data:', response.status);
-                        return;
-                    }
-                    let lgaDetails = await response.json();
+          let clickedLgaName = feature.properties.ABB_NAME;
+          let exampleLga = lgaDetails.find(lga => lga['LGA Name'].trim().toUpperCase() === clickedLgaName.trim().toUpperCase()) || {};
+          
+          let popupContent = `
+            <strong>LGA Name:</strong> ${exampleLga['LGA Name'] || 'N/A'}<br>
+            <strong>Total LGA Accidents:</strong> ${exampleLga['Total LGA Accidents'] || 'N/A'}<br>
+            <strong>Postcode with highest no. accidents:</strong> ${exampleLga['Postcode with highest no. accidents'] || 'N/A'}
+          `;
 
-                    // Extract clicked LGA name
-                    let clickedLgaName = feature.properties.vic_lga__3;
-
-                    // Find matching LGA details from fetched data
-                    let exampleLga = lgaDetails.find(lga => lga['LGA Name'] === clickedLgaName) || {};
-
-                    // Create popup content
-                    let popupContent = `
-                        <strong>LGA Name:</strong> ${exampleLga['LGA Name'] || 'N/A'}<br>
-                        <strong>Total LGA Accidents:</strong> ${exampleLga['Total LGA Accidents'] || 'N/A'}<br>
-                        <strong>Postcode with highest no. accidents:</strong> ${exampleLga['Postcode with highest no. accidents'] || 'N/A'}
-                    `;
-                    // Display popup
-                    L.popup()
-                      .setLatLng(event.latlng)
-                      .setContent(popupContent)
-                      .openOn(mymap);
-                }
-            });
+          // Update the popup content for the click event and reopen
+          layer.setPopupContent(popupContent);
+          layer.openPopup();
         }
-    }).addTo(mymap);
+      });
+    }
+  }).addTo(mymap);
 });
+        
 async function drawBarChart(filterBy) {
     let apiEndpoint = "";
     let xLabel = "";
